a32e29a51e1eb07e4e4d363bf1c4dff367ba72d9
Camilo Martínez <camilomartinezsuarez@gmail.com>
2021-09-10 01:09:04 -0500

    Ahora funcionan las frases palíndromo

--- Ejercicios semana 1/Ejercicio 2.dart
+++ Ejercicios semana 1/Ejercicio 2.dart
@@ -1,10 +1,10 @@
 //Validar si una cadena es un palíndromo o no
 
 bool esPalindromo(String text){
-  return text.toLowerCase().split('').reversed.join() == text.toLowerCase(); //La cadena la separo en una lista letra por letra, hago reversa la lista y luego uno la lista de nuevo como un String. Después comparo ambas palabras en minúscula.
-}
+  return text.toLowerCase().split(' ').join().split('').reversed.join() == text.toLowerCase().split(' ').join().split('').join(); //La cadena la separo en una lista letra por letra, hago reversa la lista y luego uno la lista de nuevo como un String. Después comparo ambas palabras en minúscula.
+} //Cuando es una frase le quito los espacios
 void main(){
-  String cadena = "Reconocer";
+  String cadena = "Anita lava la tina";
   print("$cadena ${esPalindromo(cadena) ? "sí" : "no"} es palíndromo");
 }
-//output: Reconocer sí es palíndromo
+//output: Anita lava la tina sí es palíndromo
